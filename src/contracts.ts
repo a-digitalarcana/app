bcc5f89999a77429d5be3058c5f0e22fd1deab77
a-digitalarcana <a@digitalarcana.xyz>
2021-12-18 23:16:36 -0800

    Switched indexerUrl to use https

--- src/contracts.ts
+++ src/contracts.ts
@@ -2,6 +2,6 @@ import { NetworkType } from "@airgap/beacon-sdk";
 
 export const fa2Contract = "KT1N1a7TA1rEedQo2pEQXhuVgSQNvgRWKkdJ";
 export const escrowContract = "KT1WZY4nrsHbQn6VHX6Ny1X1LYcPb7mss9iK";
-export const indexerUrl = "http://api.hangzhou2net.tzkt.io/v1/contracts/";
+export const indexerUrl = "https://api.hangzhou2net.tzkt.io/v1/contracts/";
 export const rpcUrl = "https://hangzhounet.api.tez.ie/";
 export const network = NetworkType.HANGZHOUNET;
